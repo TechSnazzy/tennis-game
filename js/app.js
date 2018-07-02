6b69f59265c2a214fd1471b5efc60720a3878070
Sean Morrison <sean1972@gmail.com>
2018-07-01 22:02:07 -0700

    Added motion to the red ball with setInterval

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 var canvas;
 var canvasContext;
+var ballX = 50;
 
 window.onload = function() {
-    console.log("Hello World");
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(0,0,canvas.width,canvas.height);
-    canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(200,200,50,25);
-    canvasContext.fillStyle = 'green';
-    canvasContext.fillRect(300,200,200,200);
+
+    // setInterval(drawEverything, 1000);
+    drawEverything();
+    drawEverything();
+    drawEverything();
+}
+
+function drawEverything() {
+  ballX = ballX + 20;
+
+  console.log(ballX);
+  canvasContext.fillStyle = 'black';
+  canvasContext.fillRect(0,0,canvas.width,canvas.height);
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(225,210,200,200);
+  canvasContext.fillStyle = 'red';
+  canvasContext.fillRect(ballX,100,10,10);
 }
