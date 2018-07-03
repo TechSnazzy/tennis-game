2f499bb4d2cf1005bde0a8e23c5b3baf9404755c
Sean Morrison <sean1972@gmail.com>
2018-07-02 20:58:03 -0700

    Added functionality to make ball move back and forth and bounce off walls

--- js/app.js
+++ js/app.js
@@ -1,25 +1,39 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballSpeedX = 15;
 
 window.onload = function() {
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d');
+  canvas = document.getElementById('gameCanvas');
+  canvasContext = canvas.getContext('2d');
 
-    // setInterval(drawEverything, 1000);
+  var framesPerSecond = 30;
+  setInterval(function() {
+    moveEverything();
     drawEverything();
-    drawEverything();
-    drawEverything();
+  }, 1000/framesPerSecond);
+}
+
+function moveEverything() {
+  ballX = ballX + ballSpeedX;
+  if (ballX < 0) {
+    ballSpeedX = -ballSpeedX;
+  }
+  if (ballX > canvas.width) {
+    ballSpeedX = -ballSpeedX;
+  }
 }
 
 function drawEverything() {
-  ballX = ballX + 20;
-
-  console.log(ballX);
   canvasContext.fillStyle = 'black';
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
   canvasContext.fillStyle = 'white';
-  canvasContext.fillRect(225,210,200,200);
+  canvasContext.fillRect(0,210,10,100);
   canvasContext.fillStyle = 'red';
   canvasContext.fillRect(ballX,100,10,10);
+}
+
+function colorRect(leftX, topY, width, height, drawColor) {
+  canvasContext.fillStyle = drawColor;
+  canvasContext.fillRect(leftX, topY, width, height);
 }
